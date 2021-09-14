5423af7782f3985a3eab34b1176cb30b08fafe62
beluga <49064698+Ranjeetk489@users.noreply.github.com>
2021-09-14 16:01:35 +0530

    applied .getElementById

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#text-Input");
 var output = document.querySelector("#output");
+var data = document.getElementById("para").innerHTML;
 
 console.log(txtInput);
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("Encountered a Clicked!");
-    output.innerText = "tanslated: "+ txtInput.value;
+    console.log("Encountered a Click!");
+    document.getElementById("dummy").innerHTML = data;
 });
